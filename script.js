ad1ed51611c697d90f929790e1896b04d87fc4cc
MirellaMelo27 <mirellamelo100@gmail.com>
2025-05-06 12:43:43 -0300

    quase tudo, sem o javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const imagem = document.getElementById("img")
+
+document.getElementById("volta").addEventListener("click", function(){
+    imagem.src = "image/mizu.jpg"
+})
+
+document.getElementById("vai").addEventListener("click",  function(){
+    imagem.src = "image/minamizu.jpg"
+})
+
