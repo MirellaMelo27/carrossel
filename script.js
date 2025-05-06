d884bdc21217efeedc8d6a3e0e11bb50edafdede
MirellaMelo27 <mirellamelo100@gmail.com>
2025-05-06 16:05:37 -0300

    slider

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const imagem = document.getElementById("img")
+const images = document.querySelectorAll('#imagens img')
+let slider = 0
 
-document.getElementById("volta").addEventListener("click", function(){
-    imagem.src = "image/mizu.jpg"
+function percorrendo(index){
+    images.forEach((img, i) => {
+        img.classList.toggle('ativa', i === index)
+    })
+}
+
+document.getElementById('vai').addEventListener('click', () => {
+    slider = (slider + 1) % images.length
+    percorrendo(slider)
 })
 
-document.getElementById("vai").addEventListener("click",  function(){
-    imagem.src = "image/minamizu.jpg"
+document.getElementById('volta').addEventListener('click', () =>{
+    slider = (slider - 1 + images.length) % images.length
+    percorrendo(slider)
 })
 
+setInterval(() => {
+    slider = (slider + 1) % images.length;
+    percorrendo(slider);
+  }, 5000); 
+
+  
